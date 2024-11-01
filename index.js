3f584a6efdd419ecef99fdbbcc8656b915a3057f
vemfuckharstadvecka <sanhol.s.holm@gmail.com>
2024-11-01 20:05:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 async function fetchAndDisplayQuote() {
     try {
+        const startWeek = 41;
         // Fetch the text file
-        const response = await fetch('vem.txt');
-        const text = await response.text();
-        
-        // Split the text into lines
-        const lines = text.split('\n').filter(line => line.trim() !== '');
-        
-        // Calculate which line to show based on the week number
-        const startDate = new Date('2024-01-01'); // Set your start date
-        const currentDate = new Date();
-        const timeDiff = currentDate - startDate;
-        const weekNumber = Math.floor(timeDiff / (7 * 24 * 60 * 60 * 1000)) + 1;
-        console.log(weekNumber);
-        // Use modulo to cycle through the lines if we reach the end
-        const lineToShow = weekNumber % lines.length;
-        
-        // Display the quote
-        console.log(lines[lineToShow]);
-        document.getElementById('quote').textContent = lines[lineToShow];
+        const responseSchedule = await readFile('vem.txt');
+        const responseRoomId = await readFile('namn.txt');
+        const guardABs = responseSchedule.split('\n').filter(line => line.trim() !== '');
+        const namesWithIds = responseRoomId.split('\n').filter(line => line.trim() !== '');
+
+        const currentWorkersIds = guardABs[await whoseTurn(startWeek)].split(' ');
+        console.log(currentWorkersIds[0]);
+        console.log(currentWorkersIds[1]);
+        document.getElementById('guardA').textContent = getNameByNumber(currentWorkersIds[0], namesWithIds);
+        document.getElementById('guardB').textContent = getNameByNumber(currentWorkersIds[1], namesWithIds);
     } catch (error) {
         console.error('Error:', error);
         document.getElementById('quote').textContent = 'Error loading quote';
@@ -26,4 +19,30 @@ async function fetchAndDisplayQuote() {
 }
 
 // Call the function when page loads
+async function whoseTurn(startWeek) {
+    const startDate = new Date('2024-01-01');
+    const currentDate = new Date();
+    const timeDiff = currentDate - startDate;
+    const weekNumber = Math.floor(timeDiff / (7 * 24 * 60 * 60 * 1000)) + 1;
+    console.log(weekNumber);
+    return weekNumber-startWeek-1;
+    // Update if new week is bigger
+}
+// Read entire file
+async function readFile(filename) {
+    try {
+        const response = await fetch(filename);
+        const text = await response.text();
+        return text;
+    } catch (error) {
+        console.error('Error reading file:', error);
+        return null;
+    }
+}
+
+function getNameByNumber(number, list1) {
+    const res = list1.find(entry => entry.startsWith(number + ' ')).split(" ")[1];
+    console.log(res);
+    return res;
+}
 fetchAndDisplayQuote();
