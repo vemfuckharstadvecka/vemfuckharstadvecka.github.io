b20ba544e99d2d1c858105a60159cbbb860d4d43
vemfuckharstadvecka <sanhol.s.holm@gmail.com>
2024-11-01 20:59:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ async function fetchAndDisplayQuote() {
         const responseRoomId = await readFile('namn.txt');
         const guardABs = responseSchedule.split('\n').filter(line => line.trim() !== '');
         const namesWithIds = responseRoomId.split('\n').filter(line => line.trim() !== '');
-
-        const currentWorkersIds = guardABs[await whoseTurn(startWeek)].split(' ');
+        console.log(await whoseTurn(startWeek));
+        const currentWorkersIds = guardABs[await whoseTurn(startWeek, false)].split(' ');
+        const nextWorkersIds = guardABs[await whoseTurn(startWeek, true)].split(' ');
         console.log(currentWorkersIds[0]);
         console.log(currentWorkersIds[1]);
-        document.getElementById('guardA').textContent = getNameByNumber(currentWorkersIds[0], namesWithIds);
-        document.getElementById('guardB').textContent = getNameByNumber(currentWorkersIds[1], namesWithIds);
+        document.getElementById('guardA').textContent = "Soppor: " + getNameByNumber(currentWorkersIds[0], namesWithIds);
+        document.getElementById('guardB').textContent = "Bänk: " + getNameByNumber(currentWorkersIds[1], namesWithIds);
+        document.getElementById('nGuardA').textContent = "Nästa: " + getNameByNumber(nextWorkersIds[0], namesWithIds);
+        document.getElementById('nGuardB').textContent = "Nästa: " + getNameByNumber(nextWorkersIds[1], namesWithIds);
     } catch (error) {
         console.error('Error:', error);
         document.getElementById('quote').textContent = 'Error loading quote';
@@ -19,13 +22,19 @@ async function fetchAndDisplayQuote() {
 }
 
 // Call the function when page loads
-async function whoseTurn(startWeek) {
+async function whoseTurn(startWeek, isNext) {
     const startDate = new Date('2024-01-01');
     const currentDate = new Date();
     const timeDiff = currentDate - startDate;
     const weekNumber = Math.floor(timeDiff / (7 * 24 * 60 * 60 * 1000)) + 1;
     console.log(weekNumber);
-    return weekNumber-startWeek;
+    const res = weekNumber -startWeek;
+    if (isNext) {
+        return res+1;
+    } else {
+        return res;
+    }
+    
     // Update if new week is bigger
 }
 // Read entire file
