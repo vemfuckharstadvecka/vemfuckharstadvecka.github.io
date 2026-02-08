d0030b52fc937fc895dcfbd6c21ffc4bb1bbfe01
nielsenelin <nielsenn.elin@gmail.com>
2026-02-08 11:58:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function fetchAndDisplayQuote() {
 
 // Call the function when page loads
 async function whoseTurn(startWeek, isNext) {
-    const startDate = new Date('2024-12-30');
+    const startDate = new Date('2025-12-29');
     const currentDate = new Date();
     const timeDiff = currentDate - startDate;
     const weekNumber = Math.floor(timeDiff / (7 * 24 * 60 * 60 * 1000)) + 1;
